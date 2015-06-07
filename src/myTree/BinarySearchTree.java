2dbf5e7146263983deb8afe6dcbc837bd64b4a64
guy cao <guy.cao@bfa.org>
2015-06-06 20:39:24 -0700

    reformat indentation and method order

--- src/myTree/BinarySearchTree.java
+++ src/myTree/BinarySearchTree.java
@@ -6,52 +6,52 @@ package myTree;
  *
  */
 public class BinarySearchTree {
-  private TreeNode root;
+    private TreeNode root;
 
-  public BinarySearchTree() {
-    this.root = null;
-  }
-
-  public void insert(int value) {
-    root = insert(value, root);
-  }
-
-  private TreeNode insert(int value, TreeNode node) {
-    if (node == null) {
-      node = new TreeNode(value);
-    } else if (value < node.getValue()) {
-      node.left = insert(value, node.left);
-    } else if (value > node.getValue()) {
-      node.right = insert(value, node.right);
+    public BinarySearchTree() {
+        this.root = null;
     }
-    return node;
-  }
 
-  public TreeNode getRoot() {
-    return root;
-  }
-
-  public int getTreeHeight() {
-    return BinarySearchTree.getTreeHeight(this.root);
-  }
-
-  // STATIC FUNCTIONS /////////////////////////////////////////////////////
-  public static BinarySearchTree buildRandomTree(int numberNodes) {
-    BinarySearchTree newTree = new BinarySearchTree();
-    for (int i = 0; i < numberNodes; i++) {
-      int newValue = (int) (Math.random() * 100);
-      newTree.insert(newValue);
+    public void insert(int value) {
+        root = insert(value, root);
     }
-    return newTree;
-  }
 
-  public static int getTreeHeight(TreeNode node) {
-    if (node == null) {
-      return 0;
-    } else {
-      int leftHeight = BinarySearchTree.getTreeHeight(node.left);
-      int rightHeight = BinarySearchTree.getTreeHeight(node.right);
-      return 1 + Math.max(leftHeight, rightHeight);
+    private TreeNode insert(int value, TreeNode node) {
+        if (node == null) {
+            node = new TreeNode(value);
+        } else if (value < node.getValue()) {
+            node.left = insert(value, node.left);
+        } else if (value > node.getValue()) {
+            node.right = insert(value, node.right);
+        }
+        return node;
     }
-  }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public int getTreeHeight() {
+        return BinarySearchTree.getTreeHeight(this.root);
+    }
+
+    // STATIC FUNCTIONS /////////////////////////////////////////////////////
+    public static BinarySearchTree buildRandomTree(int numberNodes) {
+        BinarySearchTree newTree = new BinarySearchTree();
+        for (int i = 0; i < numberNodes; i++) {
+            int newValue = (int) (Math.random() * 100);
+            newTree.insert(newValue);
+        }
+        return newTree;
+    }
+
+    public static int getTreeHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            int leftHeight = BinarySearchTree.getTreeHeight(node.left);
+            int rightHeight = BinarySearchTree.getTreeHeight(node.right);
+            return 1 + Math.max(leftHeight, rightHeight);
+        }
+    }
 }
